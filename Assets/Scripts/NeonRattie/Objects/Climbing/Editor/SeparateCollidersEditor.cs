879066f165296e04f51cf5273baff00cb0ac815e
Flusk <tpflusk@gmail.com>
2018-01-21 20:27:12 +0200

    Added script for separating colliders

---
+++ Assets/Scripts/NeonRattie/Objects/Climbing/Editor/SeparateCollidersEditor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NeonRattie.Tools;
+using UnityEditor;
+using UnityEngine;
+
+namespace NeonRattie.Objects.Climbing.Editor
+{
+    [CustomEditor(typeof(SeparateColliders))]
+    public class SeparateCollidersEditor : UnityEditor.Editor
+    {
+        private SeparateColliders separate;
+
+        protected virtual void OnEnable()
+        {
+            separate = (SeparateColliders) target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            bool clicked = GUILayout.Button("Separate Colliders");
+            if (clicked)
+            {
+                separate.Separate();
+            }
+        }
+    }
+}
