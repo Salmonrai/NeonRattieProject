61ea33e26fa0b38412c1001d2a9487e721d64119
Flusk <tpflusk@gmail.com>
2018-01-27 16:07:19 +0200

    Added in game pause menu

---
+++ Assets/Scripts/NeonRattie/UI/InGame/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace NeonRattie.UI.InGame
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        [SerializeField]
+        protected KeyCode activateKeyCode;
+
+        [SerializeField]
+        private GameObject ui;
+
+        private bool active = false;
+
+        public void Resume()
+        {
+            active = !active;
+            Time.timeScale = active ? 0 : 1;
+            ui.SetActive(active);
+        }
+
+        public void Exit()
+        {
+            SceneController.Instance.LoadSceneAsync("Menu");
+        }
+
+        public void Credits()
+        {
+            SceneController.Instance.LoadSceneAsync("Credits");
+        }
+        
+        protected virtual void Update()
+        {
+            if (Input.GetKeyDown(activateKeyCode))
+            {
+                active = !active;
+                Time.timeScale = active ? 0 : 1;
+            }
+            ui.SetActive(active);
+        }
+    }
+}
